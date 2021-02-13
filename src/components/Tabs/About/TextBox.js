2e02a2fac0ca7e92708612d28e572dd618ef5ebb
emtupp <emilyc.tupper@gmail.com>
2021-02-13 12:34:28 -0500

    add lorem ipsum generator

--- src/components/Tabs/About/TextBox.js
+++ src/components/Tabs/About/TextBox.js
@@ -2,6 +2,19 @@ import React from 'react';
 import Fade from 'react-reveal/Fade';
 import TextBoxItem from './TextBoxItem';
 
+import { LoremIpsum } from "lorem-ipsum";
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
+
 export default function TextBox(props) {
   return (
     <section className='textbox'>
